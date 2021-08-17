6514bba71d7e9b31217c2a47ca8c9946f08213b8
s54197 <azri.mohamad56@gmail.com>
2021-08-17 11:06:23 +0800

    point default page to login blade

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/login-page', function() { return view('auth.login-page'); });
