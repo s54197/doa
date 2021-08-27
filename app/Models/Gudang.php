0bb89cd95fdaedd24ea030e37cfaa5378bf17cd7
amanasmuei <arahman.asmuei@tm.com.my>
2021-08-27 22:30:54 +0800

    update

--- app/Models/Gudang.php
+++ app/Models/Gudang.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Gudang extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    // User relationship
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected $fillable = [
+        'gudang_nama',
+        'gudang_no_roc',
+        'gudang_bangunan',
+        'gudang_jalan',
+        'gudang_poskod',
+        'gudang_bandar',
+        'gudang_negeri',
+        'gudang_negeri_luar_malaysia',
+        'gudang_negara',
+        'gudang_no_tel',
+        'gudang_no_faks',
+        'gudang_emel',
+        'gudang_status',
+    ];
 }
