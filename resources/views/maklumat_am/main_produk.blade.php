ff8ec25d1a96315a164d5a265db3367c96928cac
s54197 <azri.mohamad56@gmail.com>
2021-09-11 16:04:57 +0800

    add excel button to export data

--- resources/views/maklumat_am/main_produk.blade.php
+++ resources/views/maklumat_am/main_produk.blade.php
@@ -70,6 +70,8 @@
                         {{-- <button type="button" class="btn btn-primary waves-light waves-effect float-right">Tambah Syarikat</button> --}}
                         <button type="button" class="btn waves-effect waves-light btn-primary float-md-right"
                         onclick="window.location='{{ route("baru.produk") }}'">Daftar Baru</button>
+                        <button type="button" class="btn waves-effect waves-light btn-primary float-md-right btn-excel mr-1"
+                        >Excel</button>
                     </div>
                 </div>
 
@@ -144,17 +146,27 @@ Adakah anda bersetuju untuk memadam data?
 @section('local_js')
 <script>
 $(document).ready(function () {
-    $('#datatable').DataTable({
-        "responsive": false,
-        "language": {
-            "paginate": {
-                "previous": "<i class='mdi mdi-chevron-left'>",
-                "next": "<i class='mdi mdi-chevron-right'>"
-            }
-        },
+    var table = $('#datatable').DataTable({
+        lengthChange: true,
+        responsive: false,
+        scrollX: true,
+        // dom: 'Bfrtip',
+        buttons: [
+            'excel',
+        ]
+        // language: {
+        //     "paginate": {
+        //         "previous": "<i class='mdi mdi-chevron-left'>",
+        //         "next": "<i class='mdi mdi-chevron-right'>"
+        //     }
+        // },
         // "drawCallback": function () {
         //     $('.dataTables_paginate > .pagination').addClass('pagination-rounded');
         // }
+    });
+
+    $(".btn-excel").on("click", function() {
+        table.button( '.buttons-excel' ).trigger();
     });
 
     // To get pembekal id
