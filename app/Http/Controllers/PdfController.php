78c706969e0645a9b3dfebe06c29a218c68b20c1
amanasmuei <arahman.asmuei@tm.com.my>
2021-09-27 11:40:27 +0800

    Updated

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -11,26 +11,24 @@ class PdfController extends Controller
 {
     public function certificate($id){
 
-        $data_borangA = BorangA::find($id)->with('syarikat','agen','produk','perawiss');
+        $data_borangA = BorangA::find($id)->with('syarikat','agen','produk','perawiss')->get()->first();
 
-        dd($data_borangA);
-
-        foreach($data_borangA->perawiss as $data_perawis){
-            $perawis_nama = $data_perawis->perawis_nama;
+        //perawis
+        $perawisnama = [];
+        foreach(BorangA::find($id)->perawiss as $perawiss_list) {
+            array_push($perawisnama,$perawiss_list->perawis_nama);
         }
-
-        // dd($perawis_nama);
         
         $data = [
             'no_siri' => $data_borangA->borangA_sijil_no_siri,
             'no_pendaftaran' => $data_borangA->borangA_no_pendaftaran,//'LRMP.R2/8495',
             'pendaftar' => $data_borangA->syarikat->syarikat_nama,//'THOR SPECIALITIES SDN. BHD.',
             'nama_dagangan' => $data_borangA->produk->produk_nama,//'ACTICIDE LA 5008',
-            'perawis_aktif' => $perawis_nama_list,//'5-CHLORO-2-METHYL-4-ISOTHIAZOLIN-3-ONE',
+            'perawis_aktif' => implode(" ",$perawisnama),//'5-CHLORO-2-METHYL-4-ISOTHIAZOLIN-3-ONE',
             'kepekatan' => $data_borangA->perawis_peratusan,// + perawis_unit/perawis_unit_lain,//'3.8 %W/W',
             'perumusan' => $data_borangA->borangA_perawis_perumusan,//'CECAIR (AL)',
             'kelas' => $data_borangA->produk->produk_kelas_racun,//'Kelas II',
-            'penggunaan' => $data_borangA->produk_kegunaan,///produk_kegunaan_lain,//'PENGAWET',
+            'penggunaan' => $data_borangA->produk->produk_kegunaan,///produk_kegunaan_lain,//'PENGAWET',
             'tempoh_sah' => $data_borangA->borangA_tarikh_lulus.' - '.$data_borangA->borangA_tarikh_tamat,//'01-APR-21 - 31-MAC-26',
             'tarikh_sign' => $data_borangA->borangA_sijil_tarikh,//'01-APR-21',
             'pembekal' => '',//$data_borangA->borangA_pengilang.' '.$data_borangA->borangA_pengilang_kontrak.' '.$data_borangA->borangA_penginvoisan,//'01-APR-21',
--- database/migrations/2021_08_30_112929_create_borang_a_s_table.php
+++ database/migrations/2021_08_30_112929_create_borang_a_s_table.php
@@ -15,16 +15,16 @@ class CreateBorangASTable extends Migration
     {
         Schema::create('borang_a_s', function (Blueprint $table) {
             $table->id();
-            // $table->string('borangA_syarikat');
-            // $table->string('borangA_agen');
+            
             $table->date('borangA_tarikh_terima_kaunter');
             $table->date('borangA_tarikh_lulus');
             $table->date('borangA_tarikh_tamat');
             $table->string('borangA_wakil_syarikat');
             $table->string('borangA_sijil_no_siri')->nullable();
             $table->string('borangA_jenis_pendaftaran');
-            // $table->string('borangA_dagangan');
+            
             $table->string('borangA_no_pendaftaran');
+            
             $table->boolean('borangA_perniagaan_mengimport');
             $table->boolean('borangA_perniagaan_mengilang');
             $table->boolean('borangA_perniagaan_lain')->nullable();
@@ -38,20 +38,21 @@ class CreateBorangASTable extends Migration
             $table->boolean('borangA_mengilang_membuat')->default(false);
             $table->string('borangA_mengilang_lain')->nullable();
             $table->string('borangA_mengilang_lain_maklumat')->nullable();
-            // $table->string('borangA_pengilang_pembekal');
-            // $table->string('borangA_pengilang_kontrak');
-            // $table->string('borangA_penginvoisan');
-            // $table->string('borangA_gudang');
-            // $table->string('borangA_perawis_aktif');
-            $table->string('borangA_perawis_kod');
+            
             $table->string('borangA_perawis_perumusan');
             $table->string('borangA_perawis_perumusan_lain')->nullable();
-            $table->string('borangA_nama_surat')->nullable();
-            $table->string('borangA_directory_surat')->nullable();
-            $table->string('borangA_nama_sijil')->nullable();
-            $table->string('borangA_directory_sijil')->nullable();
-            // $table->string('borangA_perawis_pengilang');
 
+            $table->string('borangA_sijil_no_siri')->nullable();
+            $table->string('borangA_sijil_tarikh')->nullable();
+            $table->string('borangA_sijil_fail_nama')->nullable();
+            $table->string('borangA_sijil_fail_src')->nullable();
+            $table->string('borangA_surat_no_rujukan_1')->nullable();
+            $table->string('borangA_surat_no_rujukan_2')->nullable();
+            $table->string('borangA_surat_tarikh')->nullable();
+            $table->string('borangA_surat_resit_bayaran')->nullable();
+            $table->string('borangA_surat_fail_nama')->nullable();
+            $table->string('borangA_surat_fail_src')->nullable();
+            
             $table->string('borangA_status');
 
             // Relation for user
